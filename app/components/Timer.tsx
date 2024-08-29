d410e5eac4de232fc32fcf9ec56c6f8c40ddad39
Jaga Santagostino <jaga@typefully.com>
2024-08-29 09:59:54 +0200

    .

--- app/components/Timer.tsx
+++ app/components/Timer.tsx
@@ -1,14 +1,20 @@
 import MotionNumber from "motion-number";
 import { ComponentProps, useEffect, useState } from "react";
 
-const format: ComponentProps<typeof MotionNumber>["format"] = {
-  minimumIntegerDigits: 2,
+const numberProps: Partial<ComponentProps<typeof MotionNumber>> = {
+  format: { minimumIntegerDigits: 2 },
+  style: { fontSize: 20 },
+  transition: {
+    layout: { type: "spring", duration: 0.7, bounce: 0 },
+    y: { type: "spring", duration: 0.7, bounce: 0.25 },
+    opacity: { duration: 0.7, ease: "easeOut", times: [0, 0.3] },
+  },
 };
 
 export function Timer() {
   const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(59);
+  const [seconds, setSeconds] = useState(30);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -29,36 +35,12 @@ export function Timer() {
   }, [minutes, seconds]);
 
   return (
-    <div className="font-mono text-[1.40rem] flex">
-      <MotionNumber
-        transition={{
-          layout: { type: "spring", duration: 0.7, bounce: 0 },
-          y: { type: "spring", duration: 0.7, bounce: 0.25 },
-          opacity: { duration: 0.7, ease: "easeOut", times: [0, 0.3] },
-        }}
-        value={hours}
-        format={format}
-      />
+    <div className="font-mono  flex">
+      <MotionNumber value={hours} {...numberProps} />
       <div>:</div>
-      <MotionNumber
-        transition={{
-          layout: { type: "spring", duration: 0.7, bounce: 0 },
-          y: { type: "spring", duration: 0.7, bounce: 0.25 },
-          opacity: { duration: 0.7, ease: "easeOut", times: [0, 0.3] },
-        }}
-        value={minutes}
-        format={format}
-      />
+      <MotionNumber value={minutes} {...numberProps} />
       <div>:</div>
-      <MotionNumber
-        transition={{
-          layout: { type: "spring", duration: 0.7, bounce: 0 },
-          y: { type: "spring", duration: 0.7, bounce: 0.25 },
-          opacity: { duration: 0.7, ease: "easeOut", times: [0, 0.3] },
-        }}
-        value={seconds}
-        format={format}
-      />
+      <MotionNumber value={seconds} {...numberProps} />
     </div>
   );
 }
